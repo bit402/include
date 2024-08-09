e5879c3e4b074060711ed247163bc2cbd90cd2e9
bit402 <1160092090@qq.com>
2024-08-09 19:23:36 +0800

    c语言练习3

---
+++ test3.c
@@ -0,0 +1,82 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<windows.h>
+//int main()
+//{
+//	unsigned int i = 0;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//		Sleep(100);
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char a[1000];
+//	int i = 0;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d\n", strlen(a));
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned char i = 0;
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 9;
+//	float* pfloat = (float*)&n;
+//	printf("%d\n", n);
+//	printf("float*p=%f\n", *pfloat);
+//	*pfloat = 9.0;
+//	printf("%d\n", n);
+//	printf("float*p=%f\n", *pfloat);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	float a = 5.5;
+//	return 0;
+//}
